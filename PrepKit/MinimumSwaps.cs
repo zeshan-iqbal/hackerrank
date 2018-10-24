e92ecd21ab8ff2f066e44f777bdc01c64629fe6d
zeeshan-iqbal <link2zeshan@gmail.com>
2018-10-24 15:07:30 +0400

    RansomNote: //https://www.hackerrank.com/challenges/ctci-ransom-note/problem

---
+++ PrepKit/MinimumSwaps.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace PrepKit
+{
+    public class MinimumSwaps
+    {
+        // Complete the minimumSwaps function below.
+        public int minimumSwaps(int[] arr)
+        {
+            int numberOfSwaps = 0;
+
+            //1 3 5 2 4 6 8 -> 3
+            for (int i = 0, j = 1, length = arr.Length; i < length; i++, j++)
+            {
+                if (arr[i] == j) continue;
+
+                int k = j;
+                while (k < arr.Length && arr[k] != j)
+                {
+                    k++;
+                }
+
+                arr[k] = arr[i];
+                arr[i] = j;
+                numberOfSwaps++;
+            }
+
+            return numberOfSwaps;
+        }
+        /**
+         * The idea is that if  occupies  position,  occupies  position and so on, then there will be some integer  which will occupy  position. So, this forms a cycle.
+         *So, if any element  is not at its correct position, we shift it to its correct position , then shift  to its correct position  and so on. So, if  is the length of the cycle (number of elements in the cycle), then it will require a minimum of swaps to rearrange the elements of the cycle to their correct positions.
+         *We find all such cycles and compute our answer.
+         */
+        public int minimumSwaps2(int[] arr)
+        {
+            int numberOfSwaps = 0;
+
+            //1 3 5 2 4 6 8 -> 3
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == i + 1) continue;
+
+                arr[arr[i] - 1] = arr[i];
+                arr[i] = i + 1;
+
+                numberOfSwaps++;
+            }
+
+            return numberOfSwaps;
+        }
+
+        public int swap(int[] arr, int i, int j)
+        {
+            if (arr[i] == arr[j])
+                return 1;
+
+            return 1 + swap(arr, i, j);
+        }
+
+        public void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp))
+                ;
+            int res = minimumSwaps(arr);
+
+            textWriter.WriteLine(res);
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+}
---
+++ PrepKit/RansomNote.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace PrepKit
+{
+    //https://www.hackerrank.com/challenges/ctci-ransom-note/problem
+    public class RansomNote
+    {
+        // Complete the checkMagazine function below.
+        public void checkMagazine(string[] magazine, string[] note)
+        {
+            if (magazine.Length < note.Length)
+            {
+                Console.WriteLine("No");
+                return;
+            }
+
+            Dictionary<string, int> bagOfWords = new Dictionary<string, int>();
+
+            foreach (var word in magazine)
+            {
+                if (bagOfWords.ContainsKey(word))
+                    bagOfWords[word] = bagOfWords[word] + 1;
+                else
+                    bagOfWords.Add(word, 1);
+            }
+
+            foreach (var word in note)
+            {
+                if (bagOfWords.ContainsKey(word) && bagOfWords[word] > 0)
+                    bagOfWords[word] = bagOfWords[word] - 1;
+                else
+                {
+                    Console.WriteLine("No");
+                    return;
+                }
+            }
+
+            Console.WriteLine("Yes");
+
+
+        }
+
+        public void Main(string[] args)
+        {
+            string[] mn = Console.ReadLine().Split(' ');
+
+            int m = Convert.ToInt32(mn[0]);
+
+            int n = Convert.ToInt32(mn[1]);
+
+            string[] magazine = Console.ReadLine().Split(' ');
+
+            string[] note = Console.ReadLine().Split(' ');
+
+            checkMagazine(magazine, note);
+        }
+    }
+}
